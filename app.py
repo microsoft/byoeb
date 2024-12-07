f314be99390e5753e762e4c44889949b44eac377
Rahul Sharma <rahul5982439@gmail.com>
2024-12-07 14:58:01 +0530

    Added message gates whatsapp and message queue

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ from flask import Flask, request
 from time import sleep
 import traceback
 
+from utils import is_older_than_n_minutes
+
 __import__("pysqlite3")
 sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 sys.path.append("src")
@@ -66,6 +68,23 @@ def medics():
 @app.route("/webhooks", methods=["POST"])
 def webhook():
     body = request.json
+    if (
+        body.get("object")
+        and body.get("entry")
+        and body["entry"][0].get("changes")
+        and body["entry"][0]["changes"][0].get("value")
+        and body["entry"][0]["changes"][0]["value"].get("messages")
+        and body["entry"][0]["changes"][0]["value"]["messages"][0]
+    ):
+        timestamp = body["entry"][0]["changes"][0]["value"]["messages"][0]["timestamp"]
+        n = 2
+        if is_older_than_n_minutes(int(timestamp), n=n):
+            logger.add_log(
+                action_type="Old message",
+                details={"message": f"Message older than {n} minutes"},
+                timestamp=datetime.now(),
+            )
+            return "OK", 200
     # adding request to queue
     print("Adding message to queue, ", body)
     queue_client.send_message(json.dumps(body))
@@ -168,6 +187,13 @@ def process_queue():
             messages = queue_client.receive_messages(messages_per_page=1, visibility_timeout=5)
             for message in messages:
                 try:
+                    if message.dequeue_count > 1:
+                        logger.add_log(
+                            event_name="app_error",
+                            details={"message": message.content, "error": "Dequeue count exceeded"},
+                        )
+                        queue_client.delete_message(message)
+                        continue
                     print("Message received", message.content)
                     body = json.loads(message.content)
                     print("Processing new message")
